0eff1578c3eceb17656f097a42bf601ba7e02ae3
Tiffany Kalin <tkalin@untangle.com>
2021-02-08 14:28:21 -0700

    Waf-23: don't use hostname manager for nginx

--- sync/nginx/__init__.py
+++ sync/nginx/__init__.py
@@ -1,4 +1,2 @@
-from . import hosts_manager
-
 from . import settings_manager
 from . import operations
--- sync/nginx/operations.py
+++ sync/nginx/operations.py
@@ -1,4 +1,1 @@
 import sync.registrar
-
-sync.registrar.register_operation("update-hostname",    None, ["hostname -F /etc/hostname"],          1, "restart-networking")
-sync.registrar.register_operation("restart-networking", None, ["echo 'restart-networking'"], 10, None)

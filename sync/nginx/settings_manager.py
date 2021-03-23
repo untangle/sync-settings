b42818a2d3e7c14e18da487c6483f08cf11ed0e8
Tiffany Kalin <tkalin@untangle.com>
2021-03-23 15:21:25 -0600

    waf-61: comment out enabled services file

--- sync/nginx/settings_manager.py
+++ sync/nginx/settings_manager.py
@@ -52,7 +52,7 @@ class SettingsManager(Manager):
     def sanitize_settings(self, settings_file):
         """santize settings sets settings to defaults that are set but not enabled"""
         print("%s: Sanitizing settings" % self.__class__.__name__)
-        print("http://"+self.enabled_services_hostname+":"+self.enabled_services_port+self.enabled_services_url)
+        """
         response = requests.get("https://127.0.0.1:8585/api/license/enabledservices")
 
         if response.status_code != 200:
@@ -78,6 +78,7 @@ class SettingsManager(Manager):
                         settings_file.settings = services.set_settings_value(settings_file.settings, service_settings_pieces, default)
         else:
             print("Defaults do not exist yet, might be creating them")
+        """
 
     def sync_settings(self, settings_file, prefix, delete_list):
         """syncs settings"""
